77066a48e3d5875256db8bef4b677692edd01dc0
koro74 <koro74@naver.com>
2025-01-28 01:26:54 +0900

    [Spring Boot 4][코딩연습] HelloWorld
    이슈사항 : 코딩연습
    Hello World 코딩

---
+++ bootCoding/ch01/isf.spring/src/test/java/com/apress/isf/spring/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apress.isf.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
