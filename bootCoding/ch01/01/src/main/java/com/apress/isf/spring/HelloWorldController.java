ab4ca8bd084512ff13342304713df2b6d7c4dd9b
koro74 <koro74@naver.com>
2025-01-28 03:32:06 +0900

    [Spring Boot4][코딩연습] 스프링 부트: 더 쉬운 스프링
    [이슈사항] 코딩연습
    Hello World Controller

---
+++ bootCoding/ch01/01/src/main/java/com/apress/isf/spring/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.apress.isf.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+	@GetMapping("/")
+	@ResponseBody()
+	String getMessage() {
+		return "<h1>Hello World!</h1?";
+	}
+	
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(HelloWorldController.class, args);
+	}
+}
